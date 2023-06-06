bfd0f756bf5c90df6921a59bee275f5e4f779b0c
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-06-06 15:03:58 -0300

    usando Q para mesclar AND e OR em consultas

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -48,7 +49,16 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |  # o i ignora o fato de ser maiúscula ou minúscula
+            Q(description__icontains=search_term),  # o pipe | significa OU
+        ),
+        is_published=True,
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
         'search_term': search_term,
+        'recipes': recipes,
     })
