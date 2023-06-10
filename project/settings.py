59172a5954a804d4e37a35d25dccf36d8e52efef
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-06-10 17:34:15 -0300

    created app authors

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
     'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
