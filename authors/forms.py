2e2e7f69bc7b37d2644576e640f7331abedef7bf
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-06-11 17:38:37 -0300

    labels, widgets, errors, etc

--- authors/forms.py
+++ authors/forms.py
@@ -5,4 +5,34 @@ from django.contrib.auth.models import User
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', ]
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
+        labels = {
+            'fist_name': 'First Name',
+            'last_name': 'Username',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid.',
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your First Name here',
+                'class': 'input text-input',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            }),
+        }
