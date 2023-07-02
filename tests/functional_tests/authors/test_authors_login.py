1a4860818321347e5032c91639dfa552f9f93b41
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-02 17:07:25 -0300

    increasing coverage score

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -33,5 +33,7 @@ class AuthorsLoginTest(AuthorsBaseTest):
             self.browser.find_element(By.TAG_NAME, "body").text,
         )
 
-        # end test
-        self.sleep()
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(self.live_server_url + reverse("authors:login_create"))
+
+        self.assertIn("Not Found", self.browser.find_element(By.TAG_NAME, "body").text)
