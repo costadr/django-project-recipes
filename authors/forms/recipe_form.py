e3deed3a58333910962312e3abfe52de5850273b
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-03 20:43:44 -0300

    correcting the layout

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -16,3 +16,23 @@ class AuthorRecipeForm(forms.ModelForm):
         fields = 'title', 'description', 'preparation_time', \
             'preparation_time_unit', 'servings', 'servings_unit', \
             'preparation_steps', 'cover'
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Pedaços', 'Pedaços'),
+                    ('Pessoas', 'Pessoas'),
+                ),
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos', 'Minutos'),
+                    ('Horas', 'Horas'),
+                ),
+            )
+        }
