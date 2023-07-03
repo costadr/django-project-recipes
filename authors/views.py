1dce037d1d565519b6fa78c93c93318de91563d6
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-03 13:35:18 -0300

    created the author's dashboard

--- authors/views.py
+++ authors/views.py
@@ -55,7 +55,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse("authors:login")
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -69,11 +68,10 @@ def login_create(request):
         else:
             messages.error(request, "Invalid credentials")
 
-        return redirect(login_url)
     else:
         messages.error(request, "Invalid username or password")
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 
 @login_required(login_url="authors:login", redirect_field_name="next")
@@ -89,3 +87,8 @@ def logout_view(request):
     messages.success(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse("authors:login"))
+
+
+@login_required(login_url="authors:login", redirect_field_name="next")
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
