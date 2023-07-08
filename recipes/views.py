0288cd7cd250187f44d7a65b44de32a8416a6a2b
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-08 18:23:24 -0300

    Annotate to create fields / values in the QuerySet

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
 import os
 
-from django.db.models import Q
+from django.db.models import F, Q, Value
 from django.db.models.aggregates import Count
+from django.db.models.functions import Concat
 from django.forms.models import model_to_dict
 from django.http import Http404, JsonResponse
 from django.shortcuts import get_object_or_404, render
@@ -15,7 +16,14 @@ PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def theory(request, *args, **kwargs):
-    recipes = Recipe.objects.values('id', 'title').filter(title__icontains='v')
+    recipes = Recipe.objects.all().annotate(
+        author_full_name=Concat(
+            F('author__first_name'), Value(' '), F('author__last_name'),
+            Value(' ('),
+            F('author__username'),
+            Value(')'),
+        )
+    )[:5]
     number_of_recipes = recipes.aggregate(number=Count('id'))
 
     context = {
