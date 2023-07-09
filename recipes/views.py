06d9b2c11e71f89411f580b3f790fb3f13c07405
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-08 21:59:37 -0300

    Templates, Urls and View for tags

--- recipes/views.py
+++ recipes/views.py
@@ -7,6 +7,7 @@ from django.http import Http404, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, ListView
 
+from tag.models import Tag
 from utils.pagination import make_pagination
 
 from .models import Recipe
@@ -43,6 +44,7 @@ class RecipeListViewBase(ListView):
             is_published=True,
         )
         qs = qs.select_related('author', 'category')
+        qs = qs.prefetch_related('tags')  # many to many relationship
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -94,6 +96,29 @@ class RecipeListViewCategory(RecipeListViewBase):
 
         ctx.update({
             'title': f'{ctx.get("recipes")[0].category.name} - Category | '
+        })
+        return ctx
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(tags__slug=self.kwargs.get('slug', ''))
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(slug=self.kwargs.get('slug', '')).first()
+
+        if not page_title:
+            page_title = 'No recipes found'
+
+        page_title = f'{page_title} - Tag '
+
+        ctx.update({
+            'page_title': page_title,
         })
         return ctx
 
