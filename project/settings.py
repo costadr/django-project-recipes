6c6c6730934a505da7b54f953bf99bc5d43a4e88
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-09 15:24:48 -0300

    Correcting erros in the PyTest

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+if os.environ.get('DEBUG', None) is None:
+    from dotenv import load_dotenv
+    load_dotenv()
+
 from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
