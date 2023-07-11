526795d6559b6784534d78d20fcf326b0f106362
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-11 10:34:42 -0300

    using serializers.SerializerMethodField

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,3 +5,10 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_published')  # field public does not exist in the model
+    preparation = serializers.SerializerMethodField(
+        method_name='any_method_name'
+    )
+
+    def any_method_name(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
