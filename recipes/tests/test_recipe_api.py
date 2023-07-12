d518018e7f9fb1b5923ff52a239008bd31f5ac5b
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-12 20:24:03 -0300

    using self.client.get() to test if the API returns status 200

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -1,8 +1,14 @@
+from django.urls import reverse
 from rest_framework import test
 
 from recipes.tests.test_recipe_base import RecipeMixin
 
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
-    def test_the_test(self):
-        assert 1 == 2
+    def test_recipe_api_list_returns_status_code_200(self):
+        api_url = reverse('recipes:recipes-api-list')
+        response = self.client.get(api_url)
+        self.assertEqual(
+            response.status_code,
+            200
+        )
