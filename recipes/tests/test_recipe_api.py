2f6dc9815783a348c3735a0e099121d96e1f29cb
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-07-13 11:10:39 -0300

    creating a method to obtain the JWT Access Token

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -16,6 +16,21 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         response = self.client.get(api_url)
         return response
 
+    def get_jwt_access_token(self):
+        userdata = {
+            'username': 'user',
+            'password': 'password',
+        }
+        self.make_author(
+            username=userdata.get('username'),
+            password=userdata.get('password'),
+        )
+        response = self.client.post(
+            reverse('recipes:token_obtain_pair'),
+            {**userdata}
+        )
+        return response.data.get('access')
+
     def test_recipe_api_list_returns_status_code_200(self):
         response = self.get_recipe_api_list()
         self.assertEqual(
@@ -86,3 +101,6 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             response.status_code,
             401
         )
+
+    def test_jwt_login(self):
+        print(self.get_jwt_access_token())
